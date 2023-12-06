cd5bc6b05675e2d1bbaa7cc5804df8692ea051bd
Mohamed_Ashraaaf <mohamed.ashraaf@gmail.com>
2023-12-06 11:40:50 -0800

    files manager

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,39 +1,52 @@
-import { v4 as uuidv4 } from 'uuid';
-import sha1 from 'sha1';
-import dbClient from '../utils/db';
+const dbClient = require('../utils/db');
 
-const UsersController = {
-  async postNew(req, res) {
+class UsersController {
+  static async postNew(req, res) {
     const { email, password } = req.body;
-
     if (!email) {
-      return res.status(400).json({ error: 'Missing email' });
+      res.status(400).json({ error: 'Missing email' });
+      return;
     }
-
     if (!password) {
-      return res.status(400).json({ error: 'Missing password' });
+      res.status(400).json({ error: 'Missing password' });
+      return;
     }
-
-    const userExists = await dbClient.db.collection('users').findOne({ email });
-    if (userExists) {
-      return res.status(400).json({ error: 'Already exist' });
-    }
-
-    const hashedPassword = sha1(password);
-    const newUser = {
-      email,
-      password: hashedPassword,
-    };
 
     try {
-      const result = await dbClient.db.collection('users').insertOne(newUser);
-      const { _id: id } = result.ops[0];
+      const userExists = await dbClient.getUserByEmail(email);
+      if (userExists) {
+        res.status(400).json({ error: 'Already exist' });
+        return;
+      }
 
-      res.status(201).json({ id, email });
-    } catch (err) {
-      return res.status(500).json({ error: 'Server error' });
+      const newUser = await dbClient.createUser(email, password);
+      res.status(201).json({ id: newUser.id, email: newUser.email });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Server error' });
     }
-  },
-};
+  }
 
-export default UsersController;
+  static async getMe(req, res) {
+    const { userId } = req;
+    if (!userId) {
+      res.status(401).json({ error: 'Unauthorized' });
+      return;
+    }
+
+    try {
+      const user = await dbClient.getUserById(userId);
+      if (!user) {
+        res.status(401).json({ error: 'Unauthorized' });
+        return;
+      }
+
+      res.status(200).json({ id: user.id, email: user.email });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Server error' });
+    }
+  }
+}
+
+module.exports = UsersController;
